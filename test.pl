4e1f00b10fb6aec29c86b8ebedf2c094f31fdb37
ckbullock410 <ckbullock410@gmail.com>
2018-04-09 21:09:36 -0600

    Fully functional Algorithm(slowish)

--- test.pl
+++ test.pl
@@ -19,15 +19,15 @@ main :-
 
 storeSolutions(9):- !. 		%6 should be 8
 storeSolutions(X):-
-	isPair(X,B),
-	asserta(solutionPair(X,B)),
-	A is X+1,
-	storeSolutions(A).
+	isPair(X,B),!,
+	asserta(solutionPair(X,B)),!,
+	A is X+1,!,
+	storeSolutions(A),!.
 
-printSol(9):- 
+printSol(9):-
 	bestValue(A),
 	write('Quality: '), write(A), nl,!.
-printSol(X):- 
+printSol(X):-
 	solutionPair(X,A),
 	write(X), write(':'), write(A), write("   "),
 	B is X+1,
@@ -42,139 +42,144 @@ algorithm(1,9):-  		%6 should be 8
 
 algorithm(9,_):-					%6 should be 8
 	%new solution is found
-	isPair(8,A),					%5 should be 7
-	retractall(solutionPairs(_,_)),
-	storeSolutions(1),
-	retractall(bestValue(_)),
-	curPenalty(B),
-	asserta(bestValue(B)),
-	retract(unavailable(A)),
-	comboPen(8,A,C),				%5 should be 7
-	D is B-C,
-	retract(curPenalty(_)),
-	asserta(curPenalty(D)),
-	retract(isPair(8,A)),			%5 should be 7
-	E is A+1,
-	printSol(1),
+	isPair(8,A),!,					%5 should be 7
+	retractall(solutionPairs(_,_)),!,
+	storeSolutions(1),!,
+	retractall(bestValue(_)),!,
+	curPenalty(B),!,
+	asserta(bestValue(B)),!,
+	retract(unavailable(A)),!,
+	comboPen(8,A,C),!,				%5 should be 7
+	D is B-C,!,
+	retract(curPenalty(_)),!,
+	asserta(curPenalty(D)),!,
+	retract(isPair(8,A)),!,			%5 should be 7
+	E is A+1,!,
+	printSol(1),!,
 	algorithm(8,E),					%5 should be 7
 	!.
 
 algorithm(A,9):-					%4 should be 8
 	%go back one machine
-	B is A-1,
-	isPair(B, C),
-	D is C+1,
-	retract(unavailable(C)),
-	retract(isPair(B,C)),
-	comboPen(B,C,E),
-	curPenalty(F),
-	G is F-E,
+	B is A-1,!,
+	isPair(B, C),!,
+	D is C+1,!,
+	retract(unavailable(C)),!,
+	retract(isPair(B,C)),!,
+	comboPen(B,C,E),!,
+	curPenalty(F),!,
+	G is F-E,!,
 	retract(comboPen(B,C,_)),
 	retract(curPenalty(_)),
 	asserta(curPenalty(G)),
-	algorithm(B,D),
+	algorithm(B,D),!,
 	!.
 
 algorithm(X,Y):-
 	%check if valid combo
-	X < 9,					%6's should be 8's
+	X < 9,				%6's should be 8's
 	Y < 9,
-	isValid(X,Y),
-	asserta(isPair(X,Y)),
-	asserta(unavailable(Y)),
-	calculatePenalty(X,Y,F),
-	curPenalty(A),
-	asserta(comboPen(X,Y,F)),
-	retract(curPenalty(_)),
-	C is A+F,
-	asserta(curPenalty(C)),
-	D is X+1,
+	isValid(X,Y),!,
+  asserta(isPair(X,Y)),!,
+	asserta(isPair(X,Y)),!,
+	asserta(unavailable(Y)),!,
+	calculatePenalty(X,Y,F),!,
+	curPenalty(A),!,
+	asserta(comboPen(X,Y,F)),!,
+	retract(curPenalty(_)),!,
+	C is A+F,!,
+	asserta(curPenalty(C)),!,
+	D is X+1,!,
 	algorithm(D,1),!.
 
 algorithm(X,Y):-
-	% was invalid check next task 
-	A is Y+1,
+	% was invalid check next task
+	A is Y+1,!,
 	algorithm(X,A).
 
 
 
 isValid(X,Y):-
-	\+ (unavailable(Y)),
-	\+ (forbiddenMachines(X,Y)),
-	notTooNearTask(X,Y),
-	notForcedAnother(X,Y),
+	\+ (unavailable(Y)),!,
+	\+ (forbiddenMachines(X,Y)),!,
+	notTooNearTask(X,Y),!,
+	notForcedAnother(X,Y),!,
 	penaltyNotBigger(X,Y).
 
 notTooNearTask(1,_):-!.
 
 notTooNearTask(8,Y):-			%5 should be 7
-	isPair(1,A),
+	isPair(1,A),!,
 	\+(tooNearTasks(Y,A)),!.
 
 notTooNearTask(X,Y):-
-	A is X-1,
-	isPair(A, B),
+	A is X-1,!,
+	isPair(A, B),!,
 	\+ (tooNearTasks(B,Y)).
 
 notForcedAnother(X,Y):-
-	(\+(forcedPartialAssignments(X,_));(forcedPartialAssignments(X,A), A =:= Y)),
+	(\+(forcedPartialAssignments(X,_));(forcedPartialAssignments(X,A), A =:= Y)),!,
 	(\+(forcedPartialAssignments(_,Y));(forcedPartialAssignments(B,Y), B =:= X)).
 
 penaltyNotBigger(X,Y):-
-	calculatePenalty(X,Y,A),
-	curPenalty(B),
-	C is A+B,
-	bestValue(D),
+	calculatePenalty(X,Y,A),!,
+	curPenalty(B),!,
+	C is A+B,!,
+	bestValue(D),!,
 	C < D.
 
 calculatePenalty(X,Y,C):-
-	machinePenalties(X,Y,A),
-	checktooNearPenalties(X,Y,B),
+	machinePenalties(X,Y,A),!,
+	checktooNearPenalties(X,Y,B),!,
 	C is A+B.
 
 checktooNearPenalties(1,_,A):- A = 0, !.
 
-checktooNearPenalties(8,Y,A):-			%5 should be 7
-	\+(tooNearPenalties(Y,_,_)),
-	A = 0, !.
-
-checktooNearPenalties(8,Y,A):-			% should be 7
+checktooNearPenalties(8,Y,A):-		
 	isPair(1,B),
 	tooNearPenalties(Y,B,C),
-	A = C , !.
+	A = C,
+	!.
+checktooNearPenalties(8,Y,A):-
+	isPair(1,B),
+	\+(tooNearPenalties(Y,B,_)),
+	A = 0,!.
 
-checktooNearPenalties(X,Y,A):-
-	B is X-1,
-	isPair(B,C),
-	\+(tooNearPenalties(C,Y,_)),
-	A = 0, !.
 
 checktooNearPenalties(X,Y,A):-
 	B is X-1,
 	isPair(B,C),
 	tooNearPenalties(C,Y,D),
-	A = D.
+	A = D,
+	!.
+checktooNearPenalties(X,Y,A):-
+	B is X-1,
+	isPair(B,C),
+	\+(tooNearPenalties(C,Y,_)),
+	A = 0,!.
 
 
-forcedPartialAssignments(1,1).
+	
 
-machinePenalties(1,1,1).
+tooNearPenalties(1,2,10).
+
+machinePenalties(1,1,2).
 machinePenalties(1,2,1).
 machinePenalties(1,3,2).
 machinePenalties(1,4,2).
 machinePenalties(1,5,2).
 machinePenalties(1,6,2).
 machinePenalties(1,7,2).
-machinePenalties(1,8,1).
+machinePenalties(1,8,2).
 
-machinePenalties(2,1,1).
+machinePenalties(2,1,2).
 machinePenalties(2,2,1).
-machinePenalties(2,3,1).
-machinePenalties(2,4,1).
-machinePenalties(2,5,1).
-machinePenalties(2,6,1).
-machinePenalties(2,7,1).
-machinePenalties(2,8,1).
+machinePenalties(2,3,2).
+machinePenalties(2,4,2).
+machinePenalties(2,5,2).
+machinePenalties(2,6,2).
+machinePenalties(2,7,2).
+machinePenalties(2,8,2).
 
 machinePenalties(3,1,1).
 machinePenalties(3,2,1).
@@ -222,10 +227,10 @@ machinePenalties(7,7,1).
 machinePenalties(7,8,1).
 
 machinePenalties(8,1,1).
-machinePenalties(8,2,1).
-machinePenalties(8,3,1).
-machinePenalties(8,4,1).
-machinePenalties(8,5,1).
-machinePenalties(8,6,1).
-machinePenalties(8,7,1).
-machinePenalties(8,8,1).
+machinePenalties(8,2,2).
+machinePenalties(8,3,2).
+machinePenalties(8,4,2).
+machinePenalties(8,5,2).
+machinePenalties(8,6,2).
+machinePenalties(8,7,2).
+machinePenalties(8,8,2).
